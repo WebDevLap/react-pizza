e4500a611443fe4a9ad9e7e900e8be6ad4da67d5
WebDevLap <114002073+WebDevLap@users.noreply.github.com>
2023-01-20 16:09:20 +0300

    improve visibility

--- src/pages/carts.jsx
+++ src/pages/carts.jsx
@@ -3,9 +3,11 @@ import React from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { clearItems } from "../redux/slices/cartSlice"
 import { CartEmpty } from "../components/cartEmpty"
+import { NavLink } from "react-router-dom"
 
 export default function Carts(){
-  const pizzasInCart = useSelector(state => state.cart.pizzasInCart)
+  const {pizzasInCart, totalPrice} = useSelector(state => state.cart)
+
   const dispatch = useDispatch()
 
   function onClearClick(){
@@ -22,6 +24,16 @@ export default function Carts(){
           {
             pizzasInCart.map((item, index) => <Cart key={index} {...pizzasInCart[index]}/> )
           }
+          <div className="cart__info">
+            <div className="cart-info__content">
+              <div className="cart-info__text">Всего пицц: <span>{pizzasInCart.length} шт.</span></div>
+              <div className="cart-info__text">Сумма заказа: <span className="orange">{totalPrice} ₽</span></div>
+            </div>
+            <div className="cart-info__buttons">
+              <NavLink className='cart-info__back' to='/' onClick={() => window.scrollTo(0, 0)}>Вернуться назад</NavLink>
+              <button className='cart-info__buy'>Оплатить сейчас</button>
+            </div>
+          </div>
         </>
         : <CartEmpty/>
       }
